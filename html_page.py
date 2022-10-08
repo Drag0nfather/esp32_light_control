39068d04a1da28e8d7565bb4e1a0b6e3156b957d
ayagudin <ayagudin@ozon.ru>
2022-10-08 15:50:48 +0300

    base

---
+++ html_page.py
@@ -0,0 +1,53 @@
+home_page = """\
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="ru"><head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+<title>Главная</title>
+<style>.u-input {display: block;width: 100%;padding: 10px 12px;}</style>
+<style>.u-btn {padding: 10px 30px;}</style>
+<style>.u-btn-submit {display: inline-block; margin: 1px;}</style>
+<style>.u-align-center {text-align: center;}</style>
+<style>a {text-decoration: none;}</style>
+<style>.u-form-control-hidden {display: none !important;}</style>
+<style>.u-overlap.u-overlap-transparent:not(.u-overlap-contrast) .u-header :not(.u-nav-item) > .u-btn, .u-gradient > .u-container-layout > .u-btn, .u-image:not(.u-shading) > .u-container-layout > .u-btn, .u-btn {background-color: #478ac9;color: #ffffff;}</style>
+</head>
+<body data-home-page="Главная.html" data-home-page-title="Главная" class="u-body u-xl-mode" data-lang="ru"><header class="u-clearfix u-header u-header" id="sec-b358"><div class="u-clearfix u-sheet u-sheet-1"></div></header>
+<section class="u-clearfix u-section-1" id="sec-93b1">
+  <div class="u-clearfix u-sheet u-sheet-1">
+	<div class="u-form u-form-1">
+	  <form action="/test2" method="post" accept-charset="ISO-8859-1" class="u-clearfix u-form-spacing-10 u-form-vertical u-inner-form" name="form" style="padding: 10px;">
+		<div class="u-form-date u-form-group u-form-group-1">
+		  <label class="u-label">1</label>
+		  <input type="range" name="input1" min="0" max="1023" step="1" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+		</div>
+		<div class="u-form-date u-form-group u-form-group-2">
+			<label class="u-label">2</label>
+			<input type="range" name="input2"  min="0" max="1023" step="1" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+		</div>
+		<div class="u-form-date u-form-group u-form-group-3">
+			<label class="u-label">3</label>
+			<input type="range" name="input3" min="0" max="1023" step="1" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+		</div>
+		<div class="u-form-date u-form-group u-form-group-4">
+			<label class="u-label">4</label>
+			<input type="range" name="input4"  min="0" max="1023" step="1" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+		</div>
+		<div class="u-form-date u-form-group u-form-partition-factor-2 u-form-group-5" style="float: left; width: 45%; height: 100px;">
+		  <label for="date-fcba" class="u-label u-label-5">Время с</label>
+		  <input type="time" id="date-fcba" name="input5" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+		</div>
+		<div class="u-form-date u-form-group u-form-partition-factor-2 u-form-group-6" style="float: right; width: 45%; height: 100px;">
+		  <label for="date-52ee" class="u-label">Время до</label>
+		  <input type="time" id="date-52ee" name="input6" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+		</div>
+		<div class="u-align-center u-form-group u-form-submit" style="margin-top: 5%;">
+		  <button class="u-btn u-btn-submit u-button-style" type="submit">Сохранить</button>
+		  <input type="submit" value="Сохранить" class="u-form-control-hidden">
+		</div>
+	  </form>
+	</div>
+  </div>
+</section>
+
+</body></html>"""
---
+++ main.py
@@ -0,0 +1,119 @@
+import time
+import machine
+import network
+import ds1302
+from html_page import home_page
+from MicroWebSrv import MicroWebSrv
+import _thread
+
+
+first_pwm = ''
+second_pwm = ''
+third_pwm = ''
+fourth_pwm = ''
+
+start_time = ''
+end_time = ''
+
+light_on = False
+light_off = True
+
+wi_fi = False
+
+# rtc init
+ds = ds1302.DS1302(machine.Pin(25), machine.Pin(26), machine.Pin(27))
+
+
+# bind button to 13 pin
+button = machine.Pin(13, machine.Pin.IN)
+
+
+def start_led(pin_num, freq_num, duty_num):
+    pin = machine.Pin(pin_num)
+    pwm = machine.PWM(pin)
+    pwm.freq(freq_num)
+    pwm.duty(duty_num)
+
+
+def stop_led(pin_num):
+    pin = machine.Pin(pin_num)
+    pwm = machine.PWM(pin)
+    pwm.deinit()
+
+
+def activate_wi_fi():
+    ap = network.WLAN(network.AP_IF)
+    ap.active(True)
+    ap.config(essid='esp32', password='123123123')
+
+
+def deactivate_wi_fi():
+    ap = network.WLAN(network.AP_IF)
+    ap.active(False)
+
+
+def start_server(threaded):
+    try:
+        mws = MicroWebSrv(port=80, bindIP="192.168.4.1", webPath="/flash/www")
+        mws.MaxWebSocketRecvLen = 256
+        mws.WebSocketThreaded = False
+        mws.Start(threaded=threaded)
+    except KeyboardInterrupt:
+        pass
+
+
+@MicroWebSrv.route("/test2")
+def main_get_handler(httpClient, httpResponse):
+    content = home_page
+    httpResponse.WriteResponseOk(headers=None, contentType="text/html", contentCharset="UTF-8", content=content)
+
+
+@MicroWebSrv.route('/test2', 'POST')
+def main_post_handler(httpClient, httpResponse):
+    global first_pwm, second_pwm, third_pwm, fourth_pwm, start_time, end_time
+
+    form_data = httpClient.ReadRequestPostedFormData()
+    first_pwm = form_data["input1"]
+    second_pwm = form_data["input2"]
+    third_pwm = form_data["input3"]
+    fourth_pwm = form_data["input4"]
+    start_time = form_data["input5"]
+    end_time = form_data["input6"]
+
+    content = home_page
+    httpResponse.WriteResponseOk(headers=None, contentType="text/html", contentCharset="UTF-8", content=content)
+
+
+def loop():
+    while True:
+        global first_pwm, second_pwm, third_pwm, fourth_pwm, start_time, end_time, light_on, light_off, wi_fi
+        time.sleep(5)
+        button_state = button.value()
+        if button_state == 1 and wi_fi is False:
+            activate_wi_fi()
+            wi_fi = True
+        if button_state == 0 and wi_fi is True:
+            deactivate_wi_fi()
+            wi_fi = False
+        current_datetime_from_ds1302 = ds.date_time()
+        curr_time = f'{current_datetime_from_ds1302[4]}:{current_datetime_from_ds1302[5]}'
+        if len(curr_time) == 4:
+            curr_time = '0' + curr_time
+        if curr_time == start_time and light_off:
+            start_led(16, 50, int(first_pwm))
+            start_led(17, 50, int(second_pwm))
+            start_led(18, 50, int(third_pwm))
+            start_led(19, 50, int(fourth_pwm))
+            light_on = True
+            light_off = False
+        if curr_time == end_time and light_on:
+            stop_led(16)
+            stop_led(17)
+            stop_led(18)
+            stop_led(19)
+            light_on = False
+            light_off = True
+
+
+_thread.start_new_thread(start_server, (False,))
+_thread.start_new_thread(loop, ())
