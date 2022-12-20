1d43a02f8e50f2f2282581b72840eb8f9de85705
ayagudin <ayagudin@ozon.ru>
2022-12-20 22:56:18 +0300

    update hardware part

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import utime
 import machine
 import network
 import ds1302
-from html_page import home_page, success_setting_schedule, set_time_page, success_set_time
+from html_page import home_page, success_setting_schedule, set_time_page, success_set_time, main_page, sheduled_time_page
 from MicroWebSrv import MicroWebSrv
 import _thread
-
 
 first_pwm = ''
 second_pwm = ''
@@ -19,6 +18,8 @@ end_time = ''
 start_time_utime = ''
 end_time_utime = ''
 curr_time_utime = ''
+
+time_range = ''
 
 is_sunrise = ''
 
@@ -41,7 +42,7 @@ def start_led(pin_num, duty_num):
 def stop_led(pin_num):
     pin = machine.Pin(pin_num)
     pwm = machine.PWM(pin)
-    pwm.duty(1000)
+    pwm.freq(200000)
 
 
 def activate_wi_fi():
@@ -65,6 +66,14 @@ def start_server(threaded):
         pass
 
 
+def set_end_time(start_time, time_range):
+    global end_time
+    start_time_utime = utime.mktime((2000, 1, 2, int(start_time[:2]), int(start_time[3:]), 0, 1, 1))
+    end_time_utime = utime.gmtime(start_time_utime + time_range * 60 * 60)
+    end_time = f'{end_time_utime[3]}:{end_time_utime[4]}'
+    return end_time
+
+
 def set_utime_time(start, end):
     global start_time_utime, end_time_utime, curr_time_utime
     ds1302_time = ds.date_time()
@@ -75,13 +84,15 @@ def set_utime_time(start, end):
         curr_time = hour + ':' + minute
     if len(minute) == 1:
         curr_time = hour + ':0' + minute
-
     if int(start[:2]) < int(end[:2]):
         start_time_utime = utime.mktime((2000, 1, 2, int(start[:2]), int(start[3:]), 0, 1, 1))
         end_time_utime = utime.mktime((2000, 1, 2, int(end[:2]), int(end[3:]), 0, 1, 1))
         curr_time_utime = utime.mktime((2000, 1, 2, int(curr_time[:2]), int(curr_time[3:]), 0, 1, 1))
         if -86340 <= curr_time_utime - end_time_utime <= -84600:
             curr_time_utime = utime.mktime((2000, 1, 3, int(curr_time[:2]), int(curr_time[3:]), 0, 1, 1))
+        if 84600 <= curr_time_utime - start_time_utime <= 86340:
+            start_time_utime = time.mktime((2000, 1, 3, int(start[:2]), int(start[3:]), 0, 1, 1))
+            curr_time_utime = utime.mktime((2000, 1, 2, int(curr_time[:2]), int(curr_time[3:]), 0, 1, 1))
     elif int(start[:2]) >= int(end[:2]):
         start_time_utime = utime.mktime((2000, 1, 2, int(start[:2]), int(start[3:]), 0, 1, 1))
         end_time_utime = utime.mktime((2000, 1, 3, int(end[:2]), int(end[3:]), 0, 2, 2))
@@ -140,15 +151,61 @@ def light_by_utime_ticks(ticks, power, led_num):
         stop_led(led_num)
 
 
+@MicroWebSrv.route('/start')
+def main_page(httpClient, httpResponse):
+    content = main_page
+    httpResponse.WriteResponseOk(headers=None, contentType="text/html", contentCharset="UTF-8", content=content)
+
+
+@MicroWebSrv.route('/schedule')
+def schedule_page(httpClient, httpResponse):
+    if len(str(ds.hour())) == 1:
+        hour = '0' + str(ds.hour())
+    else:
+        hour = str(ds.hour())
+    if len(str(ds.minute())) == 1:
+        minute = '0' + str(ds.minute())
+    else:
+        minute = str(ds.minute())
+    hour_minute = f'{hour}:{minute}'
+    content = sheduled_time_page % hour_minute
+    httpResponse.WriteResponseOk(headers=None, contentType="text/html", contentCharset="UTF-8", content=content)
+
+
+@MicroWebSrv.route('/shedule', 'POST')
+def schedule_page(httpClient, httpResponse):
+    global start_time, end_time, time_range, first_pwm, second_pwm, third_pwm, fourth_pwm
+    form_data = httpClient.ReadRequestPostedFormData()
+    first_pwm = form_data["input1"]
+    second_pwm = form_data["input2"]
+    third_pwm = form_data["input3"]
+    fourth_pwm = form_data["input4"]
+    start_time = form_data["input5"]
+    mode = form_data['input7']
+    time_range = mode
+    end_time = False
+    content = success_set_time
+    httpResponse.WriteResponseOk(headers=None, contentType="text/html", contentCharset="UTF-8", content=content)
+
+
 @MicroWebSrv.route("/test2")
 def main_get_handler(httpClient, httpResponse):
-    content = home_page % ds.date_time()
+    if len(str(ds.hour())) == 1:
+        hour = '0' + str(ds.hour())
+    else:
+        hour = str(ds.hour())
+    if len(str(ds.minute())) == 1:
+        minute = '0' + str(ds.minute())
+    else:
+        minute = str(ds.minute())
+    hour_minute = f'{hour}:{minute}'
+    content = home_page % hour_minute
     httpResponse.WriteResponseOk(headers=None, contentType="text/html", contentCharset="UTF-8", content=content)
 
 
 @MicroWebSrv.route('/test2', 'POST')
 def main_post_handler(httpClient, httpResponse):
-    global first_pwm, second_pwm, third_pwm, fourth_pwm, start_time, end_time, is_sunrise
+    global first_pwm, second_pwm, third_pwm, fourth_pwm, start_time, end_time, is_sunrise, time_range
 
     form_data = httpClient.ReadRequestPostedFormData()
     first_pwm = form_data["input1"]
@@ -158,7 +215,7 @@ def main_post_handler(httpClient, httpResponse):
     start_time = form_data["input5"]
     end_time = form_data["input6"]
     is_sunrise = form_data['input7']
-
+    time_range = ''
     content = success_setting_schedule % (start_time, end_time)
     httpResponse.WriteResponseOk(headers=None, contentType="text/html", contentCharset="UTF-8", content=content)
 
@@ -181,8 +238,8 @@ def main_get_handler(httpClient, httpResponse):
 
 def loop():
     while True:
-        global first_pwm, second_pwm, third_pwm, fourth_pwm, start_time, end_time, wi_fi, is_sunrise
-        time.sleep(5)
+        global first_pwm, second_pwm, third_pwm, fourth_pwm, start_time, end_time, wi_fi, is_sunrise, time_range
+        time.sleep(0.5)
         button_state = button.value()
         if button_state == 1 and wi_fi is False:
             activate_wi_fi()
@@ -190,6 +247,8 @@ def loop():
         if button_state == 0 and wi_fi is True:
             deactivate_wi_fi()
             wi_fi = False
+        if time_range:
+            end_time = set_end_time(start_time, time_range)
         if start_time and end_time:
             set_utime_time(start_time, end_time)
             global start_time_utime, end_time_utime, curr_time_utime
